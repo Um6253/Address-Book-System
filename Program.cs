1a58192c8df7654b65a710190126bac5144c904d
Um6253 <um6253@srmist.edu.in>
2024-01-25 14:57:59 +0530

    [Mani]:Adding  Personal Information for the Address Book

--- Program.cs
+++ Program.cs
@@ -13,23 +13,47 @@ namespace Address_Book_System
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address Book");
+            Console.WriteLine("Enter your Details");
+            Console.Write("Enter Your FirstName: ");
+            string first_Name = Console.ReadLine();
+            Console.Write("Enter Your LastName: ");
+            string last_Name = Console.ReadLine();
+            Console.Write("Enter Your Address: ");
+            string address =Console.ReadLine();
+            Console.Write("Enter Your city: ");
+            string city =Console.ReadLine();
+            Console.Write("Enter Your state: ");
+            string state =Console.ReadLine();
+            Console.Write("Enter Your zipCode");
+            int zipCode =Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Your PhoneNumber ");
+            int ph_Number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Your Email ID: ");
+            String email=Console.ReadLine();
             Console.ReadLine();
         }
         public class Address_Book
         { 
-
-
-        
             public string First_Name;
             public string Last_Name;
             public string Address;
             public string City;
             public string State;
-            public int ZipCode;
+            public int ZipCode ;
             public int Ph_Number;
             public string Email;
-            
-        
+
+            public void AddDetails(string first_Name, string last_Name, string address, string city, string state, int zipCode, int ph_Number, string email)
+            {
+                this.First_Name = first_Name;
+                this.Last_Name = last_Name;
+                this.Address = address;
+                this.City = city;
+                this.State = state;
+                this.ZipCode = zipCode;
+                this.Ph_Number = ph_Number;
+                this.Email = email;
+            }
         }
 
     }

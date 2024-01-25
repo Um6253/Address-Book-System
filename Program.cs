1c287f02249b5a27dd59f98d7d22d41e2cfd2323
Um6253 <um6253@srmist.edu.in>
2024-01-25 12:35:12 +0530

    [Mani]:Creating the seperate Parametrs for Personal Information

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,25 @@ namespace Address_Book_System
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Address Book");
+            Console.ReadLine();
         }
+        public class Address_Book
+        { 
+
+
+        
+            public string First_Name;
+            public string Last_Name;
+            public string Address;
+            public string City;
+            public string State;
+            public int ZipCode;
+            public int Ph_Number;
+            public string Email;
+            
+        
+        }
+
     }
 }

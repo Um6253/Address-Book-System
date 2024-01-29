d72be98f9199b978d8aff7f3740a9813f66d907f
Um6253 <um6253@srmist.edu.in>
2024-01-29 15:35:46 +0530

    [Mani]:Deleting the  Contact in  the Address Book

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
-            string first_name, last_name, address, city, state, zipCode, email;
+            string first_name, last_name, address, city, state, zipcode, email;
             long ph_number;
             int option;
             AddressBook obj = new AddressBook();
@@ -19,35 +19,36 @@ namespace AddressBookSystem
             do
             {
                 Console.WriteLine("WELCOME TO THE ADDRESS BOOK");
-                Console.WriteLine("Enter the Operation To be Performed:");
-                Console.WriteLine("1.To Add a New Contact in Address_Book");
-                Console.WriteLine("2.To Display the Contact present in Address Book");
-                Console.WriteLine("3.To Edit the contac in Address Book");
-                Console.WriteLine("4.To Exit from the Address Book");
+                Console.WriteLine("Enter the operation to perform");
+                Console.WriteLine("1.To Add New  contact in Address_Book");
+                Console.WriteLine("2.To Display the contact in Address Book");
+                Console.WriteLine("3.TO Edit the contact in Address Book");
+                Console.WriteLine("4.TO remove the contact in Address Book");
+                Console.WriteLine("5.TO Exit from the Address Book");
                 option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         Console.Clear();
-                        Console.WriteLine("Enter the details to Add in Address Book");
-                        Console.Write("First Name: ");
+                        Console.WriteLine("Enter the details to add in Address Book");
+                        Console.Write("first name: ");
                         first_name = Console.ReadLine();
-                        Console.Write("Last Name: ");
+                        Console.Write("lastname: ");
                         last_name = Console.ReadLine();
-                        Console.Write("Address: ");
+                        Console.Write("address: ");
                         address = Console.ReadLine();
-                        Console.Write("City Name: ");
+                        Console.Write("city: ");
                         city = Console.ReadLine();
-                        Console.Write("State Name: ");
+                        Console.Write("state: ");
                         state = Console.ReadLine();
-                        Console.Write("zipCode: ");
-                        zipCode = Console.ReadLine();
-                        Console.Write("Email ID: ");
+                        Console.Write("zip: ");
+                        zipcode = Console.ReadLine();
+                        Console.Write("email: ");
                         email = Console.ReadLine();
-                        Console.Write("Phone Number: ");
+                        Console.Write("Phone number: ");
                         ph_number = Convert.ToInt64(Console.ReadLine());
                         AddressBook obj1 = new AddressBook();
-                        obj1.add(first_name, last_name, address, city, state, zipCode, ph_number, email);
+                        obj1.add(first_name, last_name, address, city, state, zipcode, ph_number, email);
                         contact.Add(obj1);
                         Console.Clear();
                         break;
@@ -59,51 +60,62 @@ namespace AddressBookSystem
                         break;
                     case 3:
                         Console.Clear();
-                        Console.Write("Enter the existing Email ID of the contact to Edit:");
+                        Console.Write("Enter the Existing email of the contact to edit:");
                         string emailToEdit = Console.ReadLine();
                         obj.edit(contact, emailToEdit);
                         Thread.Sleep(2000);
                         Console.Clear();
                         break;
+                    case 4:
+                        Console.Clear();
+                        Console.Write("Enter the Existing email of the contact to Remove from the Address Book  :");
+                        string emailToRemove = Console.ReadLine();
+                        obj.delete(contact, emailToRemove);
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                        break;
+
+
 
                 }
-            } while (option != 4);
+            } while (option != 5);
         }
         public class AddressBook
         {
-            private string First_name;
-            private string Last_name;
-            private string Address;
-            private string City;
-            private string State;
-            private string zipCode;
+            private string first_name;
+            private string last_name;
+            private string address;
+            private string city;
+            private string state;
+            private string zipcode;
             private long ph_number;
-            private string Email;
+            private string email;
 
-            public void add(string first_name, string last_name, string address, string city, string state, string zipCode, long ph_number, string email)
+            public void add(string first_name, string last_name, string address, string city, string state, string zipcode, long ph_number, string email)
             {
-                this.First_name = first_name;
-                this.Last_name = last_name;
-                this.Address = address;
-                this.City = city;
-                this.State = state;
-                this.zipCode = zipCode;
+                this.first_name = first_name;
+                this.last_name = last_name;
+                this.address = address;
+                this.city = city;
+                this.state = state;
+                this.zipcode = zipcode;
                 this.ph_number = ph_number;
-                this.Email = email;
+                this.email = email;
             }
             public void display(List<AddressBook> contact)
             {
-                foreach (var con in contact)
+                for(int i=0;i<contact.Count;i++)
                 {
-
-                    Console.WriteLine($"FirstName: {con.First_name}");
-                    Console.WriteLine($"lastname: {con.Last_name}");
-                    Console.WriteLine($"address: {con.Address}");
-                    Console.WriteLine($"city: {con.City}");
-                    Console.WriteLine($"state: {con.State}");
-                    Console.WriteLine($"zip: {con.zipCode}");
-                    Console.WriteLine($"email: {con.Email}");
-                    Console.WriteLine($"Phone number: {con.ph_number}");
+                    Console.WriteLine($"Contact{i + 1}");
+                    Console.WriteLine();
+                    Console.WriteLine($"FirstName: {contact[i].first_name}");
+                    Console.WriteLine($"lastname: {contact[i].last_name}");
+                    Console.WriteLine($"address: {contact[i].address}");
+                    Console.WriteLine($"city: {contact[i].city}");
+                    Console.WriteLine($"state: {contact[i].state}");
+                    Console.WriteLine($"zip: {contact[i].zipcode}");
+                    Console.WriteLine($"email: {contact[i].email}");
+                    Console.WriteLine($"Phone number: {contact[i].ph_number}");
                     
 
                 }
@@ -112,11 +124,13 @@ namespace AddressBookSystem
             {
                 string field = "", new_value = "";
                 int num = 0;
+                int flag = 0;
                 foreach (var con in contact)
                 {
-                    if (con.Email == email)
+                    if (con.email == email)
                     {
-                        Console.WriteLine("Enter the field name to be edited");
+                        flag = 1;
+                        Console.WriteLine("Enter the field name you want to edit");
                         field = Console.ReadLine();
                         Console.Write($"Enter the new value of the {field}:");
                         if (field == "number")
@@ -130,33 +144,59 @@ namespace AddressBookSystem
                         switch (field)
                         {
                             case "firstname":
-                                con.First_name = new_value;
+                                con.first_name = new_value;
                                 break;
                             case "lastname":
-                                con.Last_name = new_value;
+                                con.last_name = new_value;
                                 break;
                             case "address":
-                                con.Address = new_value;
+                                con.address = new_value;
                                 break;
                             case "city":
-                                con.City = new_value;
+                                con.city = new_value;
                                 break;
                             case "state":
-                                con.State = new_value;
+                                con.state = new_value;
                                 break;
                             case "zip":
-                                con.zipCode = new_value;
+                                con.zipcode = new_value;
                                 break;
                             case "phone Number":
                                 con.ph_number = num;
                                 break;
                             case "email":
-                                con.Email = new_value;
+                                con.email = new_value;
                                 break;
                         }
                     }
+
                 }
-                Console.WriteLine("Contact is not there in the AddressBook");
+                if (flag == 0)
+                {
+                   Console.WriteLine("Contact is not present in the AddressBook");
+
+                }
+
+            }
+            public void delete(List<AddressBook> contact, string email)
+            {
+                int flag = 0;
+                foreach (AddressBook b in contact)
+                {
+                    if (b.email == email)
+                    {
+                        flag = 1;
+                        contact.Remove(b);
+                        Console.WriteLine("The Contact is removed");
+                        break;
+                    }
+
+                }
+                if (flag == 0)
+                {
+                    Console.WriteLine("Contact is not there in the addressBook");
+                }
+
             }
         }
     }
